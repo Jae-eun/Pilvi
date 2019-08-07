4141452bd0658b57db3f0499f3611a1a6f7e3df1
Jae-eun <je4297@naver.com>
2019-08-07 14:41:05 +0900

    Delete function of detect current location

--- Pilvi/Weather/Main/ViewController/MainViewController.swift
+++ Pilvi/Weather/Main/ViewController/MainViewController.swift
@@ -14,8 +14,6 @@ final class MainViewController: UIViewController {
     // MARK: - IBOutlet
     
     @IBOutlet private weak var mainCollectionView: UICollectionView!
-    @IBOutlet private weak var pageControlButton: UIBarButtonItem!
-    
     
     // MARK: - Property
     
@@ -28,53 +26,35 @@ final class MainViewController: UIViewController {
     private var longitudeArray: [CLLocationDegrees]
         = UserDefaults.standard.array(forKey: "longitude") as? [CLLocationDegrees] ?? []
     private var locationManager: CLLocationManager!
-    private var currentLatitude: CLLocationDegrees?
-    private var currentLongitude: CLLocationDegrees?
+    private var placeNameArray: [String]
+        = UserDefaults.standard.array(forKey: "placeName") as? [String] ?? []
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setLocationManager()
-        
         mainCollectionView.dataSource = self
-        mainCollectionView.delegate = self
-        
-        setCurrentWeather(currentLatitude ?? 42.3601, currentLongitude ?? -71.0589)
         
         UserDefaults.standard.set(latitudeArray, forKey: "latitude")
         UserDefaults.standard.set(longitudeArray, forKey: "longitude")
         
-        updateWeather(count: 1)
-    }
-    
-    // MARK: - IBAction
-    
-    @IBAction func channelButtonDidTap(_ sender: Any) {
+        fetchWeather(count: latitudeArray.count)
         
-    }
-    
-    @IBAction func listButtonDidTap(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "List",
-                                      bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "ListViewController")
-        self.navigationController?.pushViewController(viewController,
-                                                      animated: true)
     }
     
     // MARK: - Methods
     
-    private func updateWeather(count: Int) {
+    private func fetchWeather(count: Int) {
         for index in 0..<count {
             WeatherService.shared.fetchWeather(
-                latitude: latitudeArray[index], //42.3601,//37.8267,
-                longitude: longitudeArray[index], //-71.0589,//-122.4233,
+                latitude: latitudeArray[index],
+                longitude: longitudeArray[index],
                 date: Date(),
                 success: { [weak self] result in
                     self?.weatherData.append(result)
                     self?.currentlyData.append(result.currently)
-                    self?.dailyData.append(result.daily.data[index])
+                    self?.dailyData.append(result.daily.data[0])
                     print(self?.weatherData)
                     print(self?.dailyData)
                     
@@ -87,53 +67,16 @@ final class MainViewController: UIViewController {
         }
     }
     
-    private func setLocationManager() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
-    }
-    
     private func addLocation(_ latitudeArray: [CLLocationDegrees],
-                             _ longitudeArray: [CLLocationDegrees]) {
+                             _ longitudeArray: [CLLocationDegrees],
+                             _ placeNameArray: [String]) {
         UserDefaults.standard.set(latitudeArray, forKey: "latitude")
         UserDefaults.standard.set(longitudeArray, forKey: "longitude")
+        UserDefaults.standard.set(placeNameArray, forKey: "placeName")
         UserDefaults.standard.synchronize()
     }
     
-    private func checkoutExistPlace(_ latitude: CLLocationDegrees,
-                                    _ longitude: CLLocationDegrees) -> Bool {
-        let isExist = true
-        if let latitudeArray = UserDefaults.standard
-            .array(forKey: "latitude") as? [CLLocationDegrees],
-            !latitudeArray.contains(latitude),
-            let longitudeArray = UserDefaults.standard
-                .array(forKey: "longitude") as? [CLLocationDegrees],
-            !longitudeArray.contains(longitude) {
-            return !isExist
-        } else {
-            return isExist
-        }
-        
-    }
-    
-    private func setCurrentWeather(_ latitude: CLLocationDegrees,
-                                   _ longitude: CLLocationDegrees) {
-        if checkoutExistPlace(latitude, longitude) == false {
-            latitudeArray.append(latitude)
-            longitudeArray.append(longitude)
-            addLocation(latitudeArray, longitudeArray)
-        }
-    }
-    
 }
-//if let currentlyData = self?.currentlyData,
-//    let dailyData = self?.dailyData {
-//    self?.sendDataToView(currentlyData[index], dailyData[index])
-//    print(currentlyData)
-//    print(dailyData)
-//}
 
 // MARK: - UICollectionViewDataSource
 
@@ -152,29 +95,9 @@ extension MainViewController: UICollectionViewDataSource {
             else { return UICollectionViewCell() }
         
         cell.weatherData = weatherData[indexPath.item]
+        print(placeNameArray)
+        cell.cityName = placeNameArray[indexPath.item]
         
         return cell
     }
 }
-
-extension MainViewController: UICollectionViewDelegate {
-    
-}
-
-// MARK: - CLLocationManagerDelegate
-
-extension MainViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager,
-                         didUpdateLocations locations: [CLLocation]) {
-        
-        if let coordinate = manager.location?.coordinate {
-            
-            currentLatitude = coordinate.latitude.rounded()
-            currentLongitude = coordinate.longitude.rounded()
-            print("latitude \(String(describing: currentLatitude)) longitude \(String(describing: currentLongitude))")
-        }
-        
-    }
-}
-
